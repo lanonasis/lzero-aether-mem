e5ad9b562a2a876a9d8a99de25f53bd83450b495
The Fixer <info@lanonasis.com>
2025-11-30 03:51:25 +0100

    chore: clean up turbo cache files and add vitest testing support
    - Remove 16 empty turbo cookie files (3-18.cookie)
    - Add vitest ^4.0.14 to root devDependencies
    - Update service worker revision hash

--- packages/mobile-pwa/dev-dist/sw.js
+++ packages/mobile-pwa/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-36397f1a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2ba2g6e06i4"
+    "revision": "0.9l2p0o59ur"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
